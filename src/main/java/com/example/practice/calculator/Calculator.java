6d86b943e7b326be2ef4eb6933a18abe66e69fdf
yurison <yu_ri_son@naver.com>
2023-07-28 23:44:35 +0900

    test: PostiveNumber 도입과 테스트

--- src/main/java/com/example/practice/calculator/Calculator.java
+++ src/main/java/com/example/practice/calculator/Calculator.java
@@ -1,9 +1,24 @@
 package com.example.practice.calculator;
 
+import com.example.practice.calculator.calculate.*;
+
+import java.util.List;
+
 public class Calculator {
 
-    public static int calculate(int operand1, String operator, int operand2) {
-        return ArithmeticOperator.calculate(operand1, operator, operand2);
+    private static final List<NewArithmeticOperator> arithmeticOperators = List.of(
+            new AdditionOperator(),
+            new SubtractionOperator(),
+            new MultiplicationOperator(),
+            new DivisionOperator()
+    );
+
+    public static int calculate(PositiveNumber operand1, String operator, PositiveNumber operand2) {
+        return arithmeticOperators.stream()
+                .filter(ao -> ao.supports(operator))
+                .map(ao -> ao.calculate(operand1, operand2))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("올바른 사칙연산이 아닙니다."));
     }
 
 }
--- src/test/java/com/example/practice/calculator/CalculatorTest.java
+++ src/test/java/com/example/practice/calculator/CalculatorTest.java
@@ -1,6 +1,8 @@
 package com.example.practice.calculator;
 
+import com.example.practice.calculator.calculate.PositiveNumber;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -8,14 +10,19 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
-public class CalculatorTest {
+class CalculatorTest {
 
     @DisplayName("연산을 수행한다.")
     @ParameterizedTest
     @MethodSource("formulaAndResult")
     void additionTest(int operand1, String operator, int operand2, int result) {
-        int calculateResult = Calculator.calculate(operand1, operator, operand2);
+        int calculateResult = Calculator.calculate(
+                new PositiveNumber(operand1),
+                operator,
+                new PositiveNumber(operand2)
+        );
 
         assertThat(calculateResult).isEqualTo(result);
     }
@@ -26,6 +33,14 @@ public class CalculatorTest {
                 Arguments.arguments(4, "*", 2, 8),
                 Arguments.arguments(4, "/", 2, 2)
         );
+    }
+
+    @DisplayName("나눗셈에서 0을 나누는 경우 IllegalArgumentException이 발생한다.")
+    @Test
+    void calculateExceptionTest() {
+        assertThatCode(() -> Calculator
+                .calculate(new PositiveNumber(10), "/", new PositiveNumber(0))
+        ).isInstanceOf(IllegalArgumentException.class);
     }
 
 }
