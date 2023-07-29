a806a40cb76bbb7de31bf9c99e7bb362ae4a7cfa
yurison <yu_ri_son@naver.com>
2023-07-29 11:21:49 +0900

    메인 클래스 분리

--- src/main/java/com/example/practice/PracticeApplication.java
+++ src/main/java/com/example/practice/PracticeApplication.java
@@ -1,4 +1,4 @@
-package com.example.practice.password;
+package com.example.practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
