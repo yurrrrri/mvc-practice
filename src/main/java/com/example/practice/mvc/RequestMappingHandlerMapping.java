9e679d9d3f1d185ed30d734e4a50363a03ba8988
yurison <yu_ri_son@naver.com>
2023-08-12 02:38:55 +0900

    UserListController 추가

--- src/main/java/com/example/practice/mvc/RequestMappingHandlerMapping.java
+++ src/main/java/com/example/practice/mvc/RequestMappingHandlerMapping.java
@@ -2,6 +2,7 @@ package com.example.practice.mvc;
 
 import com.example.practice.mvc.controller.Controller;
 import com.example.practice.mvc.controller.HomeController;
+import com.example.practice.mvc.controller.UserListController;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -12,6 +13,7 @@ public class RequestMappingHandlerMapping {
 
     void init() {
         mappings.put("/", new HomeController());
+        mappings.put("/users", new UserListController());
     }
 
     public Controller findHandler(String uriPath) {
---
+++ src/main/java/com/example/practice/mvc/controller/UserListController.java
@@ -0,0 +1,16 @@
+package com.example.practice.mvc.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.util.List;
+
+public class UserListController implements Controller {
+
+    @Override
+    public String handleRequest(HttpServletRequest req, HttpServletResponse resp) {
+        req.setAttribute("users", List.of());
+        return "/user/list.jsp";
+    }
+
+}
